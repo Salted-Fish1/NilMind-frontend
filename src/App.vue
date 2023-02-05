a697327890538c413b4c3ad3107528b62eeed7f6
nilexpr <just_for_register@qq.com>
2023-02-05 15:05:43 +0000

    feat(global): init

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import DemoComp from './components/DemoComp.vue'
+</script>
+
+<template>
+    <DemoComp></DemoComp>
+</template>
+
+<style scoped lang="less">
+
+</style>
